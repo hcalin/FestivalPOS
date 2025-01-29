36fa854f0629f5468e2fb96589c4b2de34ee2400
John Travolta <hcalinn@yahoo.ro>
2025-01-29 12:45:12 +0200

    Make the environment cross-compatible

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-g++ lib/**/*.cpp src/main.cpp src/**/*.c -I src/UI/ -I src/RFID/ -I lib/MFRC522 -I lib/RPi-Arduino-SPI -std=c++17 -lbcm2835 -lwiringPi -lwiringPiDev -lncurses -lsqlite3
+g++ lib/**/*.cpp src/main.cpp src/**/*.c -I src/UI/ -I src/RFID/ -I lib/MFRC522 -I lib/RPi-Arduino-SPI -std=c++17 -lbcm2835 -lncurses -lsqlite3
 
 
 
